5c16ae89977b8cc33b938ab3ab1554b10414b0f2
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2022-10-29 19:59:54 +0500

    Added initial project code

---
+++ src/screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Todo List</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default Home;
